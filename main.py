6749a58329bd5ba244e4813051720afd174ce80a
Reese Conner <820449@stma.k12.mn.us>
2021-09-13 09:43:53 -0500

    you

---
+++ main.py
@@ -0,0 +1,14 @@
+#print 1st movie
+print("godzilla king of the monsters")
+
+#print 2nd movie
+print("Thore Ragnarok")
+
+#print 3rd movie
+print("Baby Driver")
+
+#print 4th movie
+print("Godzilla vs Kong")
+
+#print 5th movie
+print("The Dark Knight Rises")

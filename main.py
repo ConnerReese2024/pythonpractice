0813303f5e496948cf71e96b1a77c5976c922960
ConnerReese2024 <820449@mystma.org>
2021-09-15 09:47:27 -0500

    function parameters

--- main.py
+++ main.py
@@ -16,3 +16,30 @@ def TopFiveMovies():
 
 print("these are my top five movies.")
 TopFiveMovies()
+
+def TopFiveShows():
+    #print the first show
+    print("One Piece")
+
+    #print the second show
+    print("Loki")
+
+    #print the third show
+    print("the time I got reincarnated as a slime")
+
+    #print the fourth show
+    print("One Punch Man")
+
+    #print the fith show
+    print("Fate")
+
+print("My Top Five Shows")
+TopFiveShows()
+
+def MyNameIs(turtle):
+
+    #output string
+    print(turtle + "is my name.")
+
+#run function
+MyNameIs("Conner")

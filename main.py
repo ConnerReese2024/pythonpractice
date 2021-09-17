4bb007021e694f431652ec7f21374f0af7015a03
ConnerReese2024 <820449@mystma.org>
2021-09-17 09:02:14 -0500

    MadLib

--- main.py
+++ main.py
@@ -42,4 +42,32 @@ def MyNameIs(turtle):
     print(turtle + "is my name.")
 
 #run function
-MyNameIs("Conner")
+MyNameIs("Conner ")
+
+def timesTwo(x):
+
+    print(x * 2) 
+
+timesTwo(30)
+timesTwo(25)
+timesTwo(76)
+
+
+def plusFive(x):
+
+    print(x + 5)
+
+plusFive(89)
+
+
+def divideTwo(x):
+
+    print(x / 2)
+
+divideTwo(98)
+
+def MadLib(a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,a11):
+
+    print("StarWars is a" + a1 + a2 + "of" + a3 + "versus evil in a" + a4 + "far far away. There are" + a5 + "battles between" + a6 + a7 + "in" + a8 + "space and" + a9 + "duels with" + a10 + "called" + a11 + "sabers.")
+
+MadLib(" entire", " universe ", " good ", " galaxy ", " big ",  " space ",  "ships ", " outer ", " smaller ", " weapons ", " light " )

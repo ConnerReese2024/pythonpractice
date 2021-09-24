eff7e628c4e65aa93beabcf9cdedb1d355441fa7
ConnerReese2024 <820449@mystma.org>
2021-09-24 09:48:26 -0500

    practice

--- main.py
+++ main.py
@@ -1,73 +1,9 @@
-def TopFiveMovies():
-    #print the first movie
-    print("Godzilla King of the monsters")
-
-    #print the second movie
-    print("Godzilla vs Kong")
-
-    #print the third movie
-    print("The Dark Knight Rises")
-
-    #print the fourth movie
-    print("Thor Raganarok")
-
-    #print the fifth movie
-    print("Baby Driver")
-
-print("these are my top five movies.")
-TopFiveMovies()
-
-def TopFiveShows():
-    #print the first show
-    print("One Piece")
-
-    #print the second show
-    print("Loki")
-
-    #print the third show
-    print("the time I got reincarnated as a slime")
-
-    #print the fourth show
-    print("One Punch Man")
-
-    #print the fith show
-    print("Fate")
-
-print("My Top Five Shows")
-TopFiveShows()
-
-def MyNameIs(turtle):
-
-    #output string
-    print(turtle + "is my name.")
-
-#run function
-MyNameIs("Conner ")
-
-def timesTwo(x):
-
-    print(x * 2) 
-
-timesTwo(30)
-timesTwo(25)
-timesTwo(76)
-
-
-def plusFive(x):
-
-    print(x + 5)
-
-plusFive(89)
-
-
-def divideTwo(x):
-
-    print(x / 2)
-
-divideTwo(98)
-
-def MadLib(a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,a11):
-
-    print("StarWars is a" + a1 + a2 + "of" + a3 + "versus evil in a" + a4 + "far far away. There are" + a5 + "battles between" + a6 + a7 + "in" + a8 + "space and" + a9 + "duels with" + a10 + "called" + a11 + "sabers.")
-
-MadLib(" entire", " universe ", " good ", " galaxy ", " big ",  " space ",  "ships ", " outer ", " smaller ", " weapons ", " light " )
+def greaterThan10(x):
+    if x > 10:
+        return "x is greater than 10."
+    elif x==10:
+        return "x equals 10."
+    else:
+        return "x is not greater than 10."
+    
+print(greaterThan10(15))

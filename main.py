a00de0e0cecdd8581d7b749119f6166767be4f26
ConnerReese2024 <820449@mystma.org>
2021-09-27 09:45:40 -0500

    py

--- main.py
+++ main.py
@@ -1,9 +1,30 @@
-def greaterThan10(x):
-    if x > 10:
-        return "x is greater than 10."
-    elif x==10:
-        return "x equals 10."
+def define(word):
+    if word == "cat":
+        return "a small domesticated carnivorous mammal with soft fur, a short snout, and retractable claws. It is widely kept as a pet or for catching mice, and many breeds have been developed."
+    if word == "dog":
+        return "a domesticated carnivorous mammal that typically has a long snout, an acute sense of smell, nonretractable claws, and a barking, howling, or whining voice."
+    if word == "fish":
+        return "a limbless cold-blooded vertebrate animal with gills and fins and living wholly in water."
+    if word == "bird":
+        return "a warm-blooded egg-laying vertebrate distinguished by the possession of feathers, wings, and a beak and (typically) by being able to fly."
+    if word == "lizard":
+        return "a reptile that typically has a long body and tail, four legs, movable eyelids, and a rough, scaly, or spiny skin."
+    if word == "chameleon":
+        return "a small slow-moving Old World lizard with a prehensile tail, long extensible tongue, protruding eyes that rotate independently, and a highly developed ability to change color."
+    if word == "tarantula":
+        return "a large hairy spider found chiefly in tropical and subtropical America, some kinds of which are able to catch small lizards, frogs, and birds."
+    if word == "snake":
+        return "a long limbless reptile which has no eyelids, a short tail, and jaws that are capable of considerable extension. Some snakes have a venomous bite."
+
+print(define("tarantula"))
+
+def define(word):
+    if word == "sword ":
+        return "a sword is a weapon with a long metal blad"
+    elif word == "axe":
+        return "a tool typically used for chopping wood"
     else:
-        return "x is not greater than 10."
-    
-print(greaterThan10(15))
+        return word + "is not defined in this dictionary"
+
+print(define("sword "))
+
